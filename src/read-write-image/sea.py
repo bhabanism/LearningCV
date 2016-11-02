643aea6150ed5cd26096fd4f1313b3334fa10468
unknown <bhabanism@gmail.com>
2016-11-02 21:31:23 +0530

    #1 Read write image and convert to grayscale

---
+++ src/read-write-image/sea.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('sea.jpg',cv2.IMREAD_GRAYSCALE)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('new_sea.jpg', img)
+
+
